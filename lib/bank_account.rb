9cc43c62826356661b25adef195fa5187113b33d
Hunter <kolasa.hunter@aol.com>
2020-11-25 00:53:40 -0500

    commit tree 85a9e32aef2395ce6065d70d00719020dd9c8ff3

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,21 @@
 class BankAccount
-
+    attr_accessor :balance, :status
+    attr_reader :name
+    def initialize(name)
+        @status = "open"
+        @name = name
+        @balance = 1000
+    end
+    def deposit(amount)
+        self.balance += amount
+    end
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+    def valid?
+        self.status == "open" && self.balance > 0 ? true : false
+    end
+    def close_account
+        self.status = "closed"
+    end
 end
